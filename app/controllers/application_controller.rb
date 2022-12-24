a4300df18478e0b1863eca78ffd0a55d05cb0f9b
kaushaltheeG <kaushalsk09@gmail.com>
2022-12-24 15:51:42 -0800

    added & tested csrf and errors handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::API
+    include ActionController::RequestForgeryProtection
+    protect_from_forgery with: :exception
+
+    rescue_from StandardError, with: :unhandled_error
+    rescue_from ActionController::InvalidAuthenticityToken,
+        with: :invalid_authenticity_token
     
-    
-    before_action :snake_case_params
+    before_action :snake_case_params, :attach_authenticity_token
 
 
     #CRRLLL 
@@ -28,25 +33,44 @@ class ApplicationController < ActionController::API
         !!current_user
     end 
 
-    # def test
-    #     if params.has_key?(:login)
-    #         login!(User.first)
-    #     elsif params.has_key?(:logout)
-    #         logout!
-    #     end
+    def test
+        if params.has_key?(:login)
+            login!(User.first)
+        elsif params.has_key?(:logout)
+            logout!
+        end
 
-    #     if current_user
-    #         render json: { user: current_user.slice('id', 'email', 'name', 'session_token') }
-    #     else
-    #         render json: ['No current user']
-    #     end
-    # end
+        if current_user
+            render json: { user: current_user.slice('id', 'email', 'name', 'session_token') }
+        else
+            render json: ['No current user']
+        end
+    end
 
 
     private
-
+    
+    def attach_authenticity_token
+        headers['X-CSRF-Token'] = masked_authenticity_token(session)
+    end
 
     def snake_case_params
-    params.deep_transform_keys!(&:underscore)
+        params.deep_transform_keys!(&:underscore)
+    end
+
+    def invalid_authenticity_token
+        render json: { message: 'Invalid authenticity token' }, status: :unprocessable_entity
+    end
+
+    def unhandled_error(error)
+        if request.accepts.first.html?
+            raise error
+        else
+            @message = "#{error.class} - #{error.message}"
+            @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
+            render 'api/errors/internal_server_error', status: :internal_server_error
+            
+            logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
+        end
     end
 end
