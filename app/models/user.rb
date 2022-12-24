b1eacbf992660d54234cb8f4438964b70bd72e93
kaushaltheeG <kaushalsk09@gmail.com>
2022-12-24 12:24:39 -0800

    added users table and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  name            :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :session_token, presence: true, uniqueness: true 
+    validates :email, length: {minimum:3, maximum:255}, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+    validates :name, presence: true 
+    validates :password, length: {in: 6..255}, allow_nil: true 
+
+    
+
 end
--- db/migrate/20221224200120_create_users.rb
+++ db/migrate/20221224200120_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :email, null: false, index: {unique: true}
+      t.string :name, null: false 
+      t.string :password_digest, null: false 
+      t.string :session_token, null: false, index: {unique: true}
 
       t.timestamps
     end
