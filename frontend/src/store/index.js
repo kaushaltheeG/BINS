512b6b66e0908a3013d2a3f0e558b356ae64f02b
kaushaltheeG <kaushalsk09@gmail.com>
2022-12-24 16:36:36 -0800

    installed frontend dependences and added redux store

---
+++ frontend/src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+
+let enhancer;
+export const rootReducer = combineReducers({})
+
+if (process.env.NODE_ENV === 'production') {
+    enhancer = applyMiddleware(thunk);
+} else {
+    const logger = require('redux-logger').default;
+    const composeEnhancers =
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+
+const configureStore = (preloadState = {}) => {
+    return createStore(rootReducer, preloadState, enhancer)
+}
+
+export default configureStore
