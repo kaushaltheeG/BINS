5d80db1a3458fdc7058e78fc303a3dfba95c9b70
kaushaltheeG <kaushalsk09@gmail.com>
2022-12-27 19:49:53 -0800

    styling message element 1/2

--- app/views/api/workareas/show.json.jbuilder
+++ app/views/api/workareas/show.json.jbuilder
@@ -2,8 +2,9 @@ json.partial! 'workarea', workarea: @workarea
 json.messages do 
     @workarea.messages.each do |message|
         json.set! message.id do 
-            json.extract! message, :id, :body, :author_id
+            json.extract! message, :id, :body, :author_id, :created_at, :updated_at
             json.author_email message.author.email
+            json.author_name message.author.name
         end 
     end 
 end 
--- frontend/src/components/DisplayWorkAreaPage/MessageList/MessageElement.jsx
+++ frontend/src/components/DisplayWorkAreaPage/MessageList/MessageElement.jsx
@@ -1,13 +1,22 @@
-
+import "./MessageElement.css"
 
 const MessageElement = ({ message }) => {
-    const { id, body, authorId, authorEmail } = message; //might need createdAt & modifiedAt
+    const { id, body, createdAt, updatedAt, authorName } = message; //might need createdAt & modifiedAt
+
 
     return (
         <>
-            <div>
-                <h4>{body}</h4>
-                <h5>{authorEmail}</h5>
+            <div className="message-container">
+                <div className="profile-icon">
+                    <button className="profile-icon"  onClick='#'>{authorName[0].toUpperCase()}</button>
+                </div>
+                <div className="author-time-body">
+                    <div className="author-time">
+                        <span className="author-name">{authorName}</span>
+                        <span className="timestamp">{updatedAt}</span>
+                    </div>
+                    <span className="message-body">{body}</span>
+                </div>
             </div>
         </>
     )
