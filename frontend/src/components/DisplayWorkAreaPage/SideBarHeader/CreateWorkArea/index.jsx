187bbb8763fa960a99fa9834d8d117f08aa8ccf2
kaushaltheeG <kaushalsk09@gmail.com>
2022-12-29 22:04:10 -0800

    Create work area btn works

--- frontend/src/components/DisplayWorkAreaPage/SideBarHeader/CreateWorkArea/index.jsx
+++ frontend/src/components/DisplayWorkAreaPage/SideBarHeader/CreateWorkArea/index.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import { useModal } from 'react-hooks-use-modal';
+import {useDispatch, useSelector} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+import { createWorkarea } from '../../../../store/workareaReducer';
 import './CreateWorkAreaForm.css'
 
 const CreateWorkArea = () => {
     const [Modal, open, close, isOpen] = useModal('root', {
         preventScroll: true,
         focusTrapOptions: {
-            clickOutsideDeactivates: true
+            clickOutsideDeactivates: false
         },
         components: {
             Overlay: () => {
@@ -27,6 +30,43 @@ const CreateWorkArea = () => {
         
     })
 
+    const [name, setName] = useState('');
+    const user = useSelector(state => state.session.user)
+    const lastWa = Object.values(user.memberships.workareas).pop();
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleCreate = (e) => {
+        e.preventDefault();
+        if (name) {
+            const data = {
+                name,
+                user_id: user.id
+            }
+            dispatch(createWorkarea(data))
+            return 
+        }
+        console.log('Unable to create work area viz form')
+    }
+
+    const handleCreateThenSwitch = (e) => {
+        e.preventDefault();
+        if (name) {
+            const data = {
+                name,
+                user_id: user.id
+            }
+            dispatch(createWorkarea(data))
+            console
+            history.push(`/client/workareas/${lastWa.id}`)
+
+            return
+        }
+        console.log('Unable to create or toggle work area viz form')
+    }
+
+
+
   
 
 
@@ -40,14 +80,14 @@ const CreateWorkArea = () => {
                     <div className="wa-form-container">
                         <form action="" onSubmit={e => e.preventDefault()} className="wa-form-inner-container">
                             <div className="create-input-container" >
-                                <input type="text" placeholder='Enter Name' className="create-input" />
+                                <input type="text" placeholder='Enter Name' className="create-input" onChange={e => setName(e.target.value)}/>
                             </div>
                             <div className="create-wa-buttons">
                                 <div className='create-wa-btn btn-spacing'>
-                                    <p className='btn-style'>Create</p>
+                                    <p className='btn-style' onClick={handleCreate}>Create</p>
                                 </div>
                                 <div className="create-and-switch-btn btn-spacing">
-                                    <p className='btn-style'>Create & Switch</p>
+                                    <p className='btn-style' onClick={handleCreateThenSwitch}>Create & Switch</p>
                                 </div>
 
                             </div>
