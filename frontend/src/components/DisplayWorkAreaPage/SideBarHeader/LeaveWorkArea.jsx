d49ad2eb6d9cdfa6e0821ef33269f88029436b23
kaushaltheeG <kaushalsk09@gmail.com>
2022-12-30 11:56:03 -0800

    Finished logout and Workarea header modal

--- frontend/src/components/DisplayWorkAreaPage/SideBarHeader/LeaveWorkArea.jsx
+++ frontend/src/components/DisplayWorkAreaPage/SideBarHeader/LeaveWorkArea.jsx
@@ -1,11 +1,44 @@
+// import e from "express";
+import { useHistory } from "react-router-dom"
+import { useState } from "react";
 
 const LeaveWorkArea = ({name}) => {
+    const [open, setOpen] = useState(false)
+    const history = useHistory();
 
+    const handleClick = (e) => {
+        console.log(e)
+        e.preventDefault();
+        setOpen(!open)
+    }
+
+    const handleLeave = (e) => {
+        e.preventDefault();
+        history.push('/client/workareas')
+    }
+
+    const handleNevermind = (e) => {
+        e.preventDefault();
+        setOpen(oldVal => !oldVal)
+    }
     return (
         <div className="create-wa-modal ">
-            <span className="wa-modal-menu-item">
+            <span className="wa-modal-menu-item" onClick={handleClick}>
                 Leave {name}
             </span>
+            {open && 
+                <>
+                <div className="wa-modal-menu-item ">
+                    <div id='wa-question'>
+                        <span className="leave-wa-question">Are you sure?</span>
+                    </div>
+                </div>
+                <div id="wa-answers">
+                    <span onClick={handleNevermind} className="wa-modal-menu-item hover-effect font-bold">Nevermind</span>
+                    <span className="wa-modal-menu-item hover-effect font-bold" id="leave-btn"onClick={handleLeave}>Lets Leave </span>
+                </div>
+                </>
+                }
         </div>
     )
 }
