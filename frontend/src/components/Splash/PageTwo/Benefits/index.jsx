2f31b2b656ad0fcb6794dbbc162e1568fd8f3dc6
kaushaltheeG <kaushalsk09@gmail.com>
2023-01-11 14:40:03 -0800

    Adding benefits table

--- frontend/src/components/Splash/PageTwo/Benefits/index.jsx
+++ frontend/src/components/Splash/PageTwo/Benefits/index.jsx
@@ -3,9 +3,20 @@ import "./Benefits.css"
 const Benefits = () => {
 
     return (
-        <div className="benefits-container">
-
-        </div>
+        <section className="o-section-benefits">
+            <div className="for-desktop-only">
+                <table className="benefits-table">
+                    <tbody>
+                        <tr>
+                            <th className="benefits-table__column-1 benefits-table__header">Benefits</th>
+                            <th className="benefits-table__spacer--1"></th>
+                            <th className="benefits-table__column-2 benefits-table__header benefits-table__tier-col">Free</th>
+                            <th className="benefits-table__spacer--1"></th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
     )
 }
 
